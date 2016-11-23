4662088fcba535b97e021afbadfb102dc95d7de9
prerit2803 <prerit28@ubuntu.ubuntu-domain>
2016-11-23 09:27:40 -0500

    getattr implemented

--- ramdisk.c
+++ ramdisk.c
@@ -1,6 +1,15 @@
+#define FUSE_USE_VERSION 26
+
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <time.h>
 #include <fuse.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define DEBUG 1
 
 typedef struct item{
 	struct stat *details;
@@ -9,32 +18,125 @@ typedef struct item{
 	char *name;
 	char *location;
 	int isFile;
-	char *content;
 }item;
 
 typedef struct fileSystemInfo{
 	long int totalSize;
 	long int freeBytes;
-	long int availableBytes;
 	long int NumberOfFiles;
 	long int NumberOfDir;
 	char *mountpoint;
-}*FSInfo;
+}FSInfo;
 
 item *head=NULL;
+FSInfo *fsinfo=NULL;
 
-void initFuse(char *mountpoint){
-	item *rootNode=(item *)malloc(sizeof(item));
-	rootNode->sibling=NULL;
-	rootNode->subDir=NULL;
-	rootNode->content=NULL;
-	rootNode->isFile=0;
-	strcpy(rootNode->name,mountpoint);
-	strcpy(rootNode->location,mountpoint);
+static int fs_getattr(const char *path, struct stat *stbuf);
+static int fs_opendir(const char *path, struct fuse_file_info *fi);
+static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,off_t offset, struct fuse_file_info *fi);
+static int fs_mkdir(const char *path, mode_t mode);
+static int fs_unlink(const char *path);
+static int fs_rmdir(const char *path);
+static int fs_open(const char *path, struct fuse_file_info *fi);
+static int fs_read(const char *path, char *buf, size_t size, off_t offset,struct fuse_file_info *fi);
+static int fs_write(const char *path, const char *buf, size_t size,off_t offset, struct fuse_file_info *fi);
+static int fs_create(const char *path , mode_t mode, struct fuse_file_info *fi);
 
+static int fs_getattr(const char *path, struct stat *stbuf){
+	stbuf->st_uid=getuid();
+	stbuf->st_gid=getgid();
+	stbuf->st_atime=time(NULL);
+	stbuf->st_mtime=time(NULL);
+
+	if (strcmp(path,"/")==0)
+	{
+		stbuf->st_mode= S_IFDIR | 0755;
+		stbuf->st_nlink=2;
+	}
+	else
+	{
+		stbuf->st_mode= S_IFREG | 0644;
+		stbuf->st_nlink=1;
+		stbuf->st_size=1024;
+	}
+	return 0;
 }
 
-static struct fuse_ops fuse_ops = {
+static int fs_opendir(const char *path, struct fuse_file_info *fi){
+	return 0;
+}
+
+static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,off_t offset, struct fuse_file_info *fi){
+	return 0;
+}
+
+static int fs_mkdir(const char *path, mode_t mode){
+#if DEBUG
+	printf("Path: %s\n", path);
+#endif
+	/*item newNode = (item *)malloc(sizeof(item));
+	newNode->sibling=NULL;
+	newNode->subDir=NULL;
+	newNode->isFile=0;
+	newNode->path=strndup(path,strlen(path));*/
+
+	return 0;
+}
+
+static int fs_unlink(const char *path){
+	return 0;
+}
+
+static int fs_rmdir(const char *path){
+	return 0;
+}
+
+static int fs_open(const char *path, struct fuse_file_info *fi){
+	return 0;
+}
+
+static int fs_read(const char *path, char *buf, size_t size, off_t offset,struct fuse_file_info *fi){
+	return 0;
+}
+
+static int fs_write(const char *path, const char *buf, size_t size,off_t offset, struct fuse_file_info *fi){
+	return 0;
+}
+
+static int fs_create(const char *path , mode_t mode, struct fuse_file_info *fi){
+	return 0;
+}
+
+static int initFuse(char *mountpoint){
+
+	item *rootNode=(item *)malloc(sizeof(item));
+	rootNode->details=(struct stat *)malloc(sizeof(stat));
+
+	rootNode->sibling=NULL;
+	rootNode->subDir=NULL;
+	rootNode->isFile=0;
+	rootNode->name = strndup(mountpoint,strlen(mountpoint));
+	rootNode->location=strndup("/",1);
+	//strcpy(rootNode->location, "/");
+	strcat(rootNode->location,mountpoint);
+#if DEBUG
+	printf("Mountpoint: %s\n", rootNode->location);
+#endif
+	rootNode->details->st_mode = S_IFDIR | 0755;
+	rootNode->details->st_nlink = 2;
+	rootNode->details->st_uid=0;
+	rootNode->details->st_gid=0;
+
+	fsinfo->freeBytes = fsinfo->totalSize - sizeof(item) - sizeof(stat);
+	if (fsinfo->freeBytes < 0)
+	{//no space
+		return -ENOSPC;
+	}
+	head=rootNode;
+	return 0;
+}
+
+static struct fuse_operations fuseOps = {
     .getattr    =   fs_getattr,
     .readdir    =   fs_readdir,
     .mkdir      =   fs_mkdir,
@@ -53,13 +155,17 @@ int main(int argc, char *argv[]){
 		printf("USAGE ERROR: ./ramdisk <mount point> <size>\n");
 		exit(EXIT_FAILURE);
 	}
-	FSInfo *fsinfo = (FSInfo *)malloc(sizeof(FSInfo));
-	fsinfo->totalSize=(long)atoi(argv[2]) * 1024 * 1024;
-	fsinfo->freeBytes=(long)atoi(argv[2]) * 1024 * 1024;
-	fsinfo->availableBytes=0;
+	fsinfo = (FSInfo *)malloc(sizeof(FSInfo));
+	fsinfo->totalSize=((long)atoi(argv[2])) * 1024 * 1024;
+	fsinfo->freeBytes=((long)atoi(argv[2])) * 1024 * 1024;
 	fsinfo->NumberOfDir=0;
 	fsinfo->NumberOfFiles=0;
-	strcpy(fsinfo->mountpoint,argv[1]);
+	fsinfo->mountpoint=strndup(argv[1],strlen(argv[1]));
+
+#if DEBUG
+	printf("Mountpoint: %s\n", fsinfo->mountpoint);
+#endif
 	initFuse(argv[1]);
-	return fuse_main(argc-1, argv, &fuse_ops, NULL);
+	argc--;
+	return fuse_main(argc, argv, &fuseOps, NULL);
 }
